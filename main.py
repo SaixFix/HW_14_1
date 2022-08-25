02ab41992b45706f2d3da6ec89dab3f5dfdfed24
SaixFix <4653623JgFyMrB>
2022-08-25 17:30:45 +0300

    rework all sql querry in f string and add function and their views, get_film_by_rating

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 import json
 import sqlite3
 
-from utils import get_film_by_title, get_film_by_select_years
+from utils import get_film_by_title, get_film_by_select_years, get_film_by_rating
 
 app = Flask(__name__)
 
@@ -30,12 +30,35 @@ def search_movie_by_years():
     b = request.args['b']
     result = get_film_by_select_years(a, b)
 
-    # if a != int or a == '':
-    #     abort(404)
-    # elif b != int or b == '':
-    #     abort(404)
-
     return result
+
+
+@app.route('/rating/children')
+def movie_by_rating_children():
+    """
+    Список фильмов с рейтингом children: G
+    """
+    return get_film_by_rating('("G")')
+
+
+@app.route('/rating/family')
+def movie_by_rating_family():
+    """
+    Список фильмов с рейтингом family: "G", "PG", "PG-13"
+    """
+    rating = ("G", "PG", "PG-13")
+    return get_film_by_rating(rating)
+
+
+@app.route('/rating/adult')
+def movie_by_rating_adult():
+    """
+    Список фильмов с рейтингом adult: "R", "NC-17"
+    """
+    rating = ("R", "NC-17")
+    return get_film_by_rating(rating)
+
+
 
 
 @app.errorhandler(404)
--- utils.py
+++ utils.py
@@ -2,61 +2,59 @@ import sqlite3
 import json
 
 
-def connection_sql_base(query, uid):
+def connection_sql_base(query):
     """
      Читаем из базы netflix.db по запросу
     """
     with sqlite3.connect('netflix.db') as connection:
         cursor = connection.cursor()
-        return cursor.execute(query, uid)
+        return cursor.execute(query).fetchall()
 
 
 def get_film_by_title(title) -> dict or None:
     """
     возвращает информацию о фильме по названию
     """
-    query = """
+    query = f"""
                     SELECT title, country, release_year, listed_in, description
                     FROM netflix
-                    WHERE title = (?)
-                    ORDER BY release_year
+                    WHERE title = {title}
+                    ORDER BY release_year DESC
                 """
-    item = connection_sql_base(query, (title,))
-    item_1 = item.fetchone()
+    item = connection_sql_base(query)
 
-    if item_1 is None:
+    if item is None:
         return "Такого фильма не найдено"
 
     film_info = {
-        "title": item_1[0],
-        "country": item_1[1],
-        "release_year": item_1[2],
-        "genre": item_1[3],
-        "description": item_1[4]
+        "title": item[0][0],
+        "country": item[0][1],
+        "release_year": item[0][2],
+        "genre": item[0][3],
+        "description": item[0][4]
     }
     return json.dumps(film_info)
 
 
 def get_film_by_select_years(from_years, to_years):
     """
-    Делает выборгу фильмоb по годам выпуска, принимая 2 значения от и до
+    Делает выборrу фильмоb по годам выпуска, принимая 2 значения от и до
     """
 
-    query = """
+    query = f"""
                         SELECT title, release_year
                         FROM netflix
-                        WHERE release_year BETWEEN (?) AND (?)
+                        WHERE release_year BETWEEN {from_years} AND {to_years}
                         ORDER BY release_year
                         LIMIT 100
                     """
-    item = connection_sql_base(query, (from_years, to_years))
-    item_1 = item.fetchall()
+    item = connection_sql_base(query)
 
-    if item_1 is None:
+    if item is None:
         return "Такого фильма не найдено"
 
     list_film = []
-    for i in item_1:
+    for i in item:
         films = {
             "title": i[0],
             "release_year": i[1],
@@ -66,7 +64,31 @@ def get_film_by_select_years(from_years, to_years):
     return json.dumps(list_film)
 
 
+def get_film_by_rating(rating):
+    """
+    Делает выборrу фильмов по заданному возрастному рейтингу
+    """
+    query = f"""
+                        SELECT title, rating, description 
+                        FROM netflix
+                        WHERE rating in {rating}
+                        ORDER BY release_year
+                    """
+    item = connection_sql_base(query)
+
+    list_film = []
+    for i in item:
+        films = {
+            "title": i[0],
+            "rating": i[1],
+            "release_year": i[2]
+        }
+        list_film.append(films)
+
+    return json.dumps(list_film)
+
+
 #
 # lol  = get_film_by_select_yers(1945, 1960)
 # # for i in lol:
-# print(lol)
+print(get_film_by_rating('("G")'))
